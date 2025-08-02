521c67ee68f68fd6fb3ab0ecc49730852dc0a6c5
Inspecting <96089121+Inspecting@users.noreply.github.com>
2025-08-01 23:41:00 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.use(cors());
-app.use(express.static(__dirname)); // serve static files like embed.html
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.send('🎬 Bilm API is running!');
@@ -12,10 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/api/embed/:imdb', (req, res) => {
   const { imdb } = req.params;
-  res.json({
-    embed: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8', // sample .m3u8 stream
-    imdb
-  });
+  res.json({ embed: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8', imdb });
 });
 
 app.listen(PORT, () => {
