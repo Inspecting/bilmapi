d26ba9a623031545e19e1851d0c07ab8fb9260a2
Inspecting <96089121+Inspecting@users.noreply.github.com>
2025-08-02 21:17:41 -0500

    Delete scrapers directory

--- scrapers/main.js
+++
@@ -1,7 +0,0 @@
-const zoro = require('./zoro');
-
-module.exports = async function(imdb) {
-  const stream = await zoro(imdb);
-  if (!stream) throw new Error("No stream found");
-  return stream;
-};
